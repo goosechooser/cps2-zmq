af69ab51e01dc8e592c8588087e805a97978991b
Marshall <xtremebass2k@gmail.com>
2017-03-22 02:22:35 -0700

    Removed old tests

--- tests/Tile_test.py
+++ tests/Tile_test.py
@@ -1,7 +1,6 @@
-import os
 from struct import Struct
 import pytest
-from cps2 import Tile, ColorTile
+from cps2 import Tile
 
 TEST_ADDR = ''
 TEST_DATA = 'F1F2F3F4F1F2F3F4F1F2F3F4F1F2F3F4F1F2F3F4F1F2F3F4F1F2F3F4F1F2F3F4'
@@ -15,7 +14,7 @@ def test_unpack():
     test_data = 'F1' * 32 + 'F2' * 32 + 'F3' * 32 + 'F4' * 32
     tile = Tile.new(TEST_ADDR, bytearray.fromhex(test_data))
 
-    tiles = [tile for tile in TILE_UNPACK_FMT.iter_unpack(tile.data)]
+    tiles = [t for t in TILE_UNPACK_FMT.iter_unpack(tile.data)]
     assert bytearray(b''.join(tiles[0])).hex() == '0f0f0f0f0000000f0f0f0f0f00000f0f' * 4
     assert bytearray(b''.join(tiles[1])).hex() == '0f0f0f0f0000000f0f0f0f0f00000f0f' * 4
     assert bytearray(b''.join(tiles[2])).hex() == '0f0f0f0f00000f000f0f0f0f000f0000' * 4
@@ -50,56 +49,6 @@ def test_from_image(tmpdir_factory):
     test_tile = Tile.from_image(str(fn2), tile.address)
 
     assert tile.data == test_tile.data
-
-# Since this was moved inside the Tile class
-@pytest.mark.skip
-@pytest.mark.parametrize("input, expected", [
-    (TEST_DATA, '0f0f0f0f00080605'),
-    (TEST_DATA16, '0f0f0f0f00080605'),
-])
-def test_bitplanes_to_tile(input, expected):
-    tile = bytearray.fromhex(input)
-
-    for row in ROW_FMT.iter_unpack(Tile._bitplanes_to_tile(tile)):
-        assert bytearray(b''.join(row)).hex() == expected
-
-# Since this was moved inside the Tile class
-@pytest.mark.skip
-@pytest.mark.parametrize("input, expected", [
-    (TEST_DATA, '0f0f0f0f00080605'),
-    (TEST_DATA16, '0f0f0f0f00080605'),
-])
-def test_tile_to_bitplanes(input, expected):
-    tile = bytearray.fromhex(TEST_DATA)
-
-    bp = Tile._bitplanes_to_tile(tile)
-    for row in ROW_FMT.iter_unpack(Tile._tile_to_bitplanes(bp)):
-        assert bytearray(b''.join(row)).hex() == 'f1f2f3f4f1f2f3f4'
-
-# Since this was moved inside the Tile class
-@pytest.mark.skip
-def test_interleave_subtiles():
-    test_data = 'F1' * 32 + 'F2' * 32 + 'F3' * 32 + 'F4' * 32
-
-    interleaved = Tile.Tile._interleave_subtiles(bytearray.fromhex(test_data))
-
-    tiles = [tile for tile in TILE_UNPACK_FMT.iter_unpack(interleaved)]
-    assert bytearray(b''.join(tiles[0])).hex() == 'f1f1f1f1f3f3f3f3f1f1f1f1f3f3f3f3' * 4
-    assert bytearray(b''.join(tiles[1])).hex() == 'f2f2f2f2f4f4f4f4f2f2f2f2f4f4f4f4' * 4
-
-# Since this was moved inside the Tile class
-@pytest.mark.skip
-def test_deinterleave_subtiles():
-    test_data = 'F1' * 32 + 'F2' * 32 + 'F3' * 32 + 'F4' * 32
-
-    interleaved = _interleave_subtiles(bytearray.fromhex(test_data))
-    deinterleaved = _deinterleave_subtiles(interleaved)
-
-    tiles = [tile for tile in TILE_PACK_FMT.iter_unpack(deinterleaved)]
-    assert bytearray(b''.join(tiles[0])).hex() == 'f1' * 32
-    assert bytearray(b''.join(tiles[1])).hex() == 'f2' * 32
-    assert bytearray(b''.join(tiles[2])).hex() == 'f3' * 32
-    assert bytearray(b''.join(tiles[3])).hex() == 'f4' * 32
 
 if __name__ == "__main__":
     print("sup")
