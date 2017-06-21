fb161b9fcf470846a1f00657199c2a63cb743b84
Marshall <xtremebass2k@gmail.com>
2017-06-21 05:00:07 -0700

    correct paths will be the end of me

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../../cps2_zmq'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- General configuration ------------------------------------------------
 
