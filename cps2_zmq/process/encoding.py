5039b87b99f8132eb5b8b21c77d396219d7fb430
Marshall <xtremebass2k@gmail.com>
2017-07-01 05:06:37 -0700

    Lets see if this resolves our circular import issues atleast

--- cps2_zmq/process/encoding.py
+++ cps2_zmq/process/encoding.py
@@ -5,7 +5,10 @@ For deserializing our objects - soon.
 """
 import importlib
 import json
-from cps2_zmq.process import Frame, Sprite, Tile
+import cps2_zmq.process
+# import cps2_zmq.process.Frame
+# from cps2_zmq.process.Sprite import Sprite
+# from cps2_zmq.process.Tile import Tile
 
 class Cps2Decoder(json.JSONDecoder):
     def __init__(self):
@@ -38,13 +41,13 @@ class Cps2Encoder(json.JSONEncoder):
     For serializing Frame, Sprite, and Tile objects.
     """
     def default(self, o):
-        if isinstance(o, Tile.Tile):
+        if isinstance(o, cps2_zmq.process.Tile.Tile):
             return handle_tile_dict(o)
 
-        if isinstance(o, Sprite.Sprite):
+        if isinstance(o, cps2_zmq.process.Sprite.Sprite):
             return handle_sprite_dict(o)
 
-        if isinstance(o, Frame.Frame):
+        if isinstance(o, cps2_zmq.process.Frame.Frame):
             dict_ = dict((k, v) for k, v in o.__dict__.items())
             dict_['sprites'] = [handle_sprite_dict(s) for s in dict_['sprites']]
             insert_module_class(dict_, o)
