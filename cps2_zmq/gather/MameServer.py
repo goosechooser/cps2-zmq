874bfc27531caae38f5295c5fda450ff90c2dfa8
Marshall <xtremebass2k@gmail.com>
2017-07-11 05:08:18 -0700

    MameServer and MameWorker refactored to use event loops and callbacks * also implemented Majordomo protocol as described in https://rfc.zeromq.org/spec:7/MDP/

--- cps2_zmq/gather/MameServer.py
+++ cps2_zmq/gather/MameServer.py
@@ -1,11 +1,15 @@
 # pylint: disable=E1101
 
+import time
 import sys
 import zmq
-from zmq.eventloop.ioloop import IOLoop
 from zmq.eventloop.zmqstream import ZMQStream
-from cps2_zmq.gather.MameWorker import MameWorker
-from cps2_zmq.gather.DBSink import DBSink
+from zmq.eventloop.ioloop import IOLoop, DelayedCallback, PeriodicCallback
+from cps2_zmq.gather import mdp
+
+HB_INTERVAL = 1000
+HB_LIVENESS = 3
+
 
 class MameServer(object):
     """
@@ -25,33 +29,70 @@ class MameServer(object):
         msgs_recv (int): Total number of messages received.
         workers (list of threads): Pool to keep track of workers.
     """
-    def __init__(self, port, toworkers):
+    WORKER_PROTOCOL = b'MDPW01'
+
+    def __init__(self, front_addr, toworkers):
         self.loop = IOLoop.instance()
         self.context = zmq.Context.instance()
-        self.port = port
 
-        self.front = self.context.socket(zmq.ROUTER)
-        self.front.bind(':'.join(["tcp://127.0.0.1", str(self.port)]))
-        # self.front.setsockopt_string(zmq.SUBSCRIBE, '')
-
+        self.frontend = self.context.socket(zmq.ROUTER)
+        self.frontend.bind(front_addr)
+        self.frontstream = None
+        
         self.backend = self.context.socket(zmq.ROUTER)
         self.backend.bind(toworkers)
-
-        self.backstream = ZMQStream(self.backend)
-        self.backstream.on_recv(self.handle_router)
+        self.backstream = None
 
         self.msgs_recv = 0
-        self.workers = []
-        self.working = True
-        self.sinks = []
+        self.workers = {}
+        self.services = {}
+        self.heartbeater = None
+        self.setup()
 
-    def cleanup(self):
+    def setup(self):
+        self.frontstream = ZMQStream(self.frontend)
+        self.frontstream.on_recv(self.handle_frontend)
+        self.frontstream.socket.setsockopt(zmq.LINGER, 0)
+
+        self.backstream = ZMQStream(self.backend)
+        self.backstream.on_recv(self.handle_backend)
+        self.backstream.socket.setsockopt(zmq.LINGER, 0)
+
+        self.heartbeater = PeriodicCallback(self.beat, HB_INTERVAL)
+        self.heartbeater.start()
+
+    def shutdown(self):
         """
         Closes all associated zmq sockets and streams.
         """
-        self.front.close()
-        self.backend.close()
-        self.backstream.close()
+        self.frontstream.on_recv(None)
+        if self.frontend:
+            # self.frontend.setsockopt(zmq.LINGER, 0)
+            self.frontend.close()
+            self.frontend = None
+
+        if self.frontstream:
+            self.frontstream.close()
+            self.frontstream = None
+
+        self.backstream.on_recv(None)
+        if self.backend:
+            # self.backend.setsockopt(zmq.LINGER, 0)
+            self.backend.close()
+            self.backend = None
+
+        if self.backstream:
+            self.backstream.close()
+            self.backstream = None
+
+        if self.heartbeater:
+            self.heartbeater.stop()
+            self.heartbeater = None
+
+        self.workers = {}
+        self.services = {}
+        self.loop.stop()
+        
 
     def start(self):
         """
@@ -59,70 +100,189 @@ class MameServer(object):
         """
         print('SERVER Starting')
         sys.stdout.flush()
-
-        for sink in self.sinks:
-            sink.start()
-
-        for worker in self.workers:
-            worker.start()
-
         
-
         self.loop.start()
-
-        self.cleanup()
 
         print('Workers have joined')
         sys.stdout.flush()
 
         print("Server Received", self.msgs_recv, "messages")
-
-    def handle_router(self, msg):
+    
+    def beat(self):
         """
-        Callback. Handles replies from workers.
+        Checks for dead workers and removes them.
         """
-        #Receives req from worker
-        worker_addr, empty, worker_msg = msg
+        for w in self.workers.values():
+            if not w.is_alive():
+                self.unregister_worker(w.idn)
 
-        #gets message from client
-        if self.working:
-            client_addr, _, client_message = self.front.recv_multipart()
+    def register_worker(self, idn, service):
+        print('Registering worker', idn)
+        sys.stdout.flush()
 
-            # 'closing' msgpacked is length 22
-            # honestly a hack, but i hate unpacking, checking, then repacking
-            if len(client_message) == 22:
-                print('client closing')
+        if idn not in self.workers:
+            self.workers[idn] = WorkerRepresentative(self.WORKER_PROTOCOL, idn, service, self.backstream)
+
+            if service in self.services:
+                print(service, 'in self.services')
                 sys.stdout.flush()
-                close_workers(self.workers, self.backend)
-                self.working = False
+                wq, wr = self.services[service]
+                wq.put(idn)
             else:
-                self.msgs_recv += 1
-                self.backend.send_multipart([worker_addr, empty, client_message])
+                print(service, 'not in self.services')
+                sys.stdout.flush()
+                q = ServiceQueue()
+                q.put(idn)
+                self.services[service] = (q, [])
+    
+    def unregister_worker(self, idn):
+        print('Unregistering worker', idn)
+        sys.stdout.flush()
+        self.workers[idn].shutdown()
+
+        service = self.workers[idn].service
+        if service in self.services:
+            wq, wr = self.services[service]
+            wq.remove(idn)
+
+        del self.workers[idn]
+
+    def disconnect_worker(self, idn, socket):
+        try:
+            socket.send_multipart([idn, b'', self.WORKER_PROTOCOL, mdp.DISCONNECT])
+        except TypeError as err:
+            print(self.__class__.__name__, 'encountered', err)
+            sys.stdout.flush()
+        self.unregister_worker(idn)
+
+    def handle_frontend(self, msg):
+        client_addr = msg.pop(0)
+        empty = msg.pop(0)
+        protocol = msg.pop(0)
+        service = msg.pop(0)
+        request = msg[0]
+        self.msgs_recv += 1
+
+        if service == b'disconnect':
+            print('Client closing. Server disconnecting workers')
+            for w in list(self.workers):
+                self.disconnect_worker(w, self.backend)
+            self.loop.stop()
+            
         else:
-            if worker_msg == b'END':
-                self.workers.pop()
+            try:
+                wq, wr = self.services[service]
+                idn = wq.get()
 
-            if not self.workers:
-                self.loop.stop()
+                if idn:
+                    self.send_request(self.backstream, idn, client_addr, request)
+                else:
+                    wr.append(request)
 
-def close_workers(workers, socket):
-    """
-    Signals to workers its time to stop.
+            except KeyError:
+                print('Received', service)
+                sys.stdout.flush()
 
-    Args:
-        workers (list): the workers to message
-        socket (:obj:`zmq.Context.socket`): the socket to send messages on
-    """
-    empty = b'empty'
-    message = b'END'
+    def handle_backend(self, msg):
+        worker_idn = msg.pop(0)
+        empty = msg.pop(0)
+        protocol = msg.pop(0)
+        command = msg.pop(0)
 
-    for worker in workers:
-        address = worker.idn
-        socket.send_multipart([address, empty, message])
+        if command == mdp.READY:
+            self.register_worker(worker_idn, msg.pop())
+
+        elif command == mdp.REPLY:
+            client_addr, _, message = msg
+            service = self.workers[worker_idn].service
+            try:
+                wq, wr = self.services[service]
+                # send it wherever
+                wq.put(worker_idn)
+                if wr:
+                    msg = wr.pop(0)
+                    self.send_request(self.backstream, worker_idn, client_addr, msg)
+
+            except KeyError:
+                print('Received', service)
+                sys.stdout.flush()
+
+        elif command == mdp.HEARTBEAT:
+            worker = self.workers[worker_idn]
+            if worker.is_alive():
+                worker.recv_heartbeat()
+
+        elif command == mdp.DISCONNECT:
+            self.unregister_worker(worker_idn)
+        
+        else:
+            self.disconnect_worker(worker_idn, self.backstream)
+
+    def send_request(self, socket, idn, client_addr, msg):
+        socket.send_multipart([idn, b'', self.WORKER_PROTOCOL, mdp.REQUEST, client_addr, b'', msg])
+
+class WorkerRepresentative(object):
+    def __init__(self, protocol, idn, service, stream):
+        self.protocol = protocol
+        self.idn = idn
+        self.service = service
+        self.current_liveness = HB_LIVENESS
+        self.stream = stream
+        self.last_heartbeat = 0
+        self.heartbeater = PeriodicCallback(self.heartbeat, HB_INTERVAL)
+        self.heartbeater.start()
+
+    def heartbeat(self):
+        self.current_liveness -= 1
+        self.stream.send_multipart([self.idn, b'', self.protocol, mdp.HEARTBEAT])
+
+    def recv_heartbeat(self):
+        self.current_liveness = HB_LIVENESS
+
+    def is_alive(self):
+        return self.current_liveness > 0
+
+    def shutdown(self):
+        self.heartbeater.stop()
+        self.heartbeater = None
+        self.stream = None
+
+class ServiceQueue(object):
+    def __init__(self):
+        self.q = []
+
+    def __contains__(self, idn):
+        return idn in self.queue
+
+    def __len__(self):
+        return len(self.q)
+
+    def remove(self, idn):
+        try:
+            self.q.remove(idn)
+        except ValueError:
+            pass
+        
+    def put(self, idn, *args, **kwargs):
+        if idn not in self.q:
+            self.q.append(idn)
+
+    def get(self):
+        if not self.q:
+            return None
+        return self.q.pop(0)
+
 
 if __name__ == '__main__':
-    server = MameServer(5556, "inproc://toworkers")
-    server.workers = [MameWorker(str(num), "inproc://toworkers", "inproc://sink") for num in range(4)]
-    server.sinks = [DBSink(str(1), "inproc://sink", 'cps2_test')]
+    from cps2_zmq.gather.MameWorker import MameWorker
+    from cps2_zmq.gather.BaseWorker import BaseWorker
+
+    # front_addr = ':'.join(["tcp://127.0.0.1:5556", str(5556)])
+    server = MameServer("tcp://127.0.0.1:5556", "tcp://127.0.0.1:5557")
+    # num = 1
+    # workers = [MameWorker(str(num), "tcp://127.0.0.1:5557", b'mame')] # for num in range(1)]
+    # for w in workers:
+    #     w.start()
 
     server.start()
+    server.shutdown()
--- tests/unit_tests/gather_tests/MameServer_test.py
+++ tests/unit_tests/gather_tests/MameServer_test.py
@@ -2,24 +2,64 @@
 
 import zmq
 import pytest
-from cps2_zmq.gather.MameServer import MameServer, close_workers
+from cps2_zmq.gather import MameServer
+from cps2_zmq.gather import mdp
 
-@pytest.fixture(scope="module")
-def test_socket():
+port = 6668
+front_addr = ':'.join(["tcp://127.0.0.1", str(port)])
+back_addr = ':'.join(["tcp://127.0.0.1", str(port + 1)])
+
+worker_idn = bytes(str(1), encoding='UTF-8')
+empty = b''
+service = b'mame'
+
+@pytest.fixture(scope="function")
+def server():
+    server = MameServer.MameServer(front_addr, back_addr)
+    yield server
+    server.shutdown()
+
+@pytest.fixture(scope="function")
+def socket():
     context = zmq.Context.instance()
     test_socket = context.socket(zmq.DEALER)
-    test_socket.bind("inproc://testsocket")
+    test_socket.setsockopt(zmq.IDENTITY, worker_idn)
+    test_socket.connect(back_addr)
     yield test_socket
     test_socket.close()
 
-@pytest.mark.timeout(timeout=10, method='thread')
-def test_close_workers(worker, test_socket):
-    worker.idn = bytes('1', encoding='UTF-8')
-    workers = [worker]
+def test_register_worker(server):
+    server.register_worker(worker_idn, service)
 
-    for w in workers:
-        w.frontend.connect("inproc://testsocket")
+    assert len(server.workers) == 1
+    assert len(server.services) == 1
 
-    close_workers(workers, test_socket)
-    for w in workers:
-        assert w.frontend.recv_multipart()
+    assert worker_idn in server.workers
+    assert service in server.services
+
+def test_unregister_worker(server):
+    server.register_worker(worker_idn, service)
+    server.unregister_worker(worker_idn)
+
+    assert len(server.workers) == 0
+    assert worker_idn not in server.workers
+
+# socket.recv_multipart() is timing out??
+@pytest.mark.skip
+@pytest.mark.timeout(timeout=3, method='thread')
+def test_disconnect_worker(server, socket):
+    server.register_worker(worker_idn, service)
+    server.disconnect_worker(worker_idn, server.backend)
+
+    print(back_addr)
+
+    result = socket.recv_multipart()
+    print('???')
+
+    assert result.pop() == mdp.DISCONNECT
+    assert result.pop() == server.WORKER_PROTOCOL
+    assert result.pop() == empty
+    assert result.pop() == worker_idn
+
+    assert len(server.workers) == 0
+    assert worker_idn not in server.workers
