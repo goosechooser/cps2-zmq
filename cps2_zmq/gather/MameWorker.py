e9b692f9fba5a80086a1264f626d71d9ac12b452
Marshall <xtremebass2k@gmail.com>
2017-07-15 00:21:11 -0700

    Now publishes processed frames.

--- cps2_zmq/gather/MameWorker.py
+++ cps2_zmq/gather/MameWorker.py
@@ -3,6 +3,8 @@
 MameWorker.py
 """
 import random
+import msgpack
+import zmq
 from cps2_zmq.gather.BaseWorker import BaseWorker
 from cps2_zmq.process import Sprite, Frame
 
@@ -21,17 +23,49 @@ class MameWorker(BaseWorker):
         working (bool): Whether the worker is in its main loop or closing.
     """
 
-def process_message(message):
-    """
-    Callback. Processes raw frame data sent by a MAME instance.
-    """
-    masked = [Sprite.sprite_mask(each) for each in message['sprites']]
-    palettes = message['palettes']
-    frame_number = message['frame_number']
-    sprites = [Sprite.from_dict(m) for m in masked]
-    result = Frame.new(frame_number, sprites, palettes)
-    return result
+    def __init__(self, idn, front_addr, service, pub_addr=None):
+        self.pub_addr = pub_addr
+        self.publish = None
+        super(MameWorker, self).__init__(idn, front_addr, service)
+
+    def setup(self):
+        if self.pub_addr:
+            context = zmq.Context.instance()
+            self.publish = context.socket(zmq.PUB)
+            self.publish.setsockopt(zmq.LINGER, 0)
+            self.publish.connect(self.pub_addr)
+
+        super(MameWorker, self).setup()
+
+    def close(self):
+        super(MameWorker, self).close()
+        if self.publish:
+            self.publish.close()
+            self.publish = None
+
+    def log_result(self, result):
+        """
+        Publishes the result.
+        """
+        if self.publish:
+            message = [b'frame', self.idn, msgpack.packb(result)]
+            self.publish.send_multipart(message)
+
+    def process(self, message):
+        """
+        Processes raw frame data sent by a MAME instance.
+        """
+        masked = [Sprite.sprite_mask(each) for each in message['sprites']]
+        palettes = message['palettes']
+        frame_number = message['frame_number']
+        sprites = [Sprite.from_dict(m) for m in masked]
+        result = Frame.new(frame_number, sprites, palettes)
+        self.log_result(result.to_json())
+
+        return result.to_json()
+
 
 if __name__ == '__main__':
-    worker = MameWorker(str(random.randint(69, 420)), "tcp://127.0.0.1:5557", b'mame')
+    worker = MameWorker(str(random.randint(69, 420)), "tcp://127.0.0.1:5557", b'mame', pub_addr="tcp://127.0.0.1:5558")
     worker.start()
+    worker.report()
