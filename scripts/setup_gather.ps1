31f8c66251ff80552737597d6e938e5b8d627888
Marshall <xtremebass2k@gmail.com>
2017-07-25 02:59:39 -0700

    Workflow/launching scripts in parallel isn't necessary

--- scripts/setup_gather.ps1
+++ scripts/setup_gather.ps1
@@ -1,33 +1,22 @@
-﻿workflow Start-Server
-{
-    param ([string] $server, [string[]] $scripts)
-    parallel {
-        sequence {
-            $server = 'D:\code\zmq_practice\cps2_zmq\gather\MameServer.py'
-            $serverID = Start-Process -FilePath "python.exe" -ArgumentList ($server) -PassThru
-            Write-Output $serverID.id $server
-
-            foreach -parallel ($script in $scripts) {
-                $pythonID = Start-Process -FilePath "python.exe" -ArgumentList ($script) -NoNewWindow -PassThru
-                Write-Output $pythonID.Id $script
-            }
-        }
-
-    }
-    Write-Output "Workflow done"
-}
+﻿
 
 $server = 'D:\code\zmq_practice\cps2_zmq\gather\MameServer.py'
-$sink = 'D:\code\zmq_practice\cps2_zmq\gather\MongoSink.py'
+$sink = 'D:\code\zmq_practice\cps2_zmq\gather\LogSink.py'
 $worker = 'D:\Code\zmq_practice\cps2_zmq\gather\MameWorker.py'
 
-$numWorkers = 4
+$numWorkers = 1
 Write-Output "Starting Server with $numWorkers workers"
-$scripts = {$sink}.Invoke()
+
+$serverID = Start-Process -FilePath "python.exe" -ArgumentList ($server) -PassThru
+Write-Output $serverID.id $server
+
+$sinkID = Start-Process -FilePath "python.exe" -ArgumentList ($sink) -PassThru
+Write-Output $sinkID.id $server
+# Start-Sleep -s 4
 
 foreach ($i in 1..$numWorkers) {
-    $scripts.Add($worker)
+    $pythonID = Start-Process -FilePath "python.exe" -ArgumentList ($worker) -PassThru
+    Write-Output $pythonID.Id $script
 }
 
-Start-Server -server $server -scripts $scripts
 Write-Output "Everything closed"
